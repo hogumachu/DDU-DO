bf050c7fcd507331dfac684bcc3f0a695b99d295
hogumachu <castle_jun@daum.net>
2023-01-22 21:57:22 +0900

    feat: add common protocols

---
+++ DDU-DO/Common/Protocol/Refreshable.swift
@@ -0,0 +1,14 @@
+//
+//  Refreshable.swift
+//  DDU-DO
+//
+//  Created by 홍성준 on 2023/01/22.
+//
+
+import Foundation
+
+protocol Refreshable {
+    
+    func refresh()
+    
+}
---
+++ DDU-DO/Common/Protocol/TopScrollable.swift
@@ -0,0 +1,14 @@
+//
+//  TopScrollable.swift
+//  DDU-DO
+//
+//  Created by 홍성준 on 2023/01/22.
+//
+
+import Foundation
+
+protocol TopScrollable {
+    
+    func scrollToTop()
+    
+}
