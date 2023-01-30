419f3484fcfff070594ac8091527bff1491e9093
hogumachu <castle_jun@daum.net>
2023-01-30 18:08:16 +0900

    feat: add home today schedule

--- DDU-DO/Home/HomeScheduleTableViewCell.swift
+++ DDU-DO/Home/HomeScheduleTableViewCell.swift
@@ -10,13 +10,46 @@ import SnapKit
 import Then
 
 struct HomeScheduleTableViewCellModel {
-    
+    let text: String?
 }
 
 final class HomeScheduleTableViewCell: UITableViewCell {
     
-    func configure(_ model: HomeScheduleTableViewCellModel) {
-        
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.setupLayout()
+        self.setupAttributes()
     }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        self.scheduleLabel.text = nil
+    }
+    
+    func configure(_ model: HomeScheduleTableViewCellModel) {
+        self.scheduleLabel.text = model.text
+    }
+    
+    private func setupLayout() {
+        self.contentView.addSubview(self.scheduleLabel)
+        self.scheduleLabel.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    private func setupAttributes() {
+        self.backgroundColor = .white
+        
+        self.scheduleLabel.do {
+            $0.textColor = .black
+            $0.lineBreakMode = .byTruncatingTail
+            $0.numberOfLines = 1
+        }
+    }
+    
+    private let scheduleLabel = UILabel(frame: .zero)
     
 }
