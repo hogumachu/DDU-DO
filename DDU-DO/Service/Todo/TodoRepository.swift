b84d6497571dae15c3b463bec1b033f6edeaae85
hogumachu <castle_jun@daum.net>
2023-10-21 17:03:16 +0900

    feat: TodoUseCase 추가

--- DDU-DO/Service/Todo/TodoRepository.swift
+++ DDU-DO/Service/Todo/TodoRepository.swift
@@ -8,6 +8,70 @@
 import Foundation
 import RealmSwift
 
+protocol TodoRepositoryType {
+    func getAll(where predicate: NSPredicate?) -> [TodoObject]
+    func insert(item: TodoObject) throws
+    func update(item: TodoObject) throws
+    func delete(item: TodoObject) throws
+    func deleteAll() throws
+}
+
+final class RealmTodoRepository: TodoRepositoryType {
+    
+    private let realm: Realm
+    
+    init(realm: Realm) {
+        self.realm = realm
+    }
+    
+    func getAll(where predicate: NSPredicate?) -> [TodoObject] {
+        var objects = realm.objects(TodoObject.self)
+        
+        if let predicate = predicate {
+            objects = objects.filter(predicate)
+        }
+        
+        return objects
+            .map { $0 }
+    }
+    
+    func insert(item: TodoObject) throws {
+        try realm.write {
+            realm.add(item)
+            self.postTodoRepositoryUpdated()
+        }
+    }
+    
+    func update(item: TodoObject) throws {
+        try delete(item: item)
+        try insert(item: item)
+        self.postTodoRepositoryUpdated()
+    }
+    
+    func delete(item: TodoObject) throws {
+        try realm.write {
+            if let object = realm.objects(TodoObject.self)
+                .filter("createdAt == %@", item.createdAt)
+                .first {
+                realm.delete(object)
+                self.postTodoRepositoryUpdated()
+            }
+        }
+    }
+    
+    func deleteAll() throws {
+        try realm.write {
+            realm.deleteAll()
+            self.postTodoRepositoryUpdated()
+        }
+    }
+    
+    private func postTodoRepositoryUpdated() {
+        NotificationCenter.default.post(name: .todoRepositoryUpdated, object: nil)
+    }
+    
+}
+
 final class TodoRepository<RepositoryObject>: Repository where RepositoryObject: Entity, RepositoryObject.ObjectType: TodoObject {
     
     typealias ObjectType = RepositoryObject.ObjectType
